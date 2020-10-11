bba36ddc73726739923021f8bf4d83f39963844c
akiakma <akiakmapro@gmail.com>
2020-10-12 00:28:35 +0900

    Updates

---
+++ precache-manifest.8b22ca19a21a2eaaa10ae38df5df3df5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1132909e81dc888f86aed7b9baf116c7",
+    "url": "/airNike/index.html"
+  },
+  {
+    "revision": "e11e59e39bba877304a6",
+    "url": "/airNike/static/css/main.930afb80.chunk.css"
+  },
+  {
+    "revision": "d39d696e09470518f5ee",
+    "url": "/airNike/static/js/2.ae17b4c5.chunk.js"
+  },
+  {
+    "revision": "625e1a2d6c607c18bcf3368aa92240d6",
+    "url": "/airNike/static/js/2.ae17b4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11e59e39bba877304a6",
+    "url": "/airNike/static/js/main.b19d4dcd.chunk.js"
+  },
+  {
+    "revision": "05f2d43335fad4d75ff6",
+    "url": "/airNike/static/js/runtime-main.a1a805bb.js"
+  },
+  {
+    "revision": "996a166aeb93e427795e5cadb0ea4450",
+    "url": "/airNike/static/media/nationale-bold.996a166a.woff2"
+  },
+  {
+    "revision": "b19ba417ee47e3959a4c5ddbb4fc1fe2",
+    "url": "/airNike/static/media/nationale-demibold.b19ba417.woff2"
+  },
+  {
+    "revision": "4b99954ff6046b83e793ff75f6bbae8c",
+    "url": "/airNike/static/media/nationale-regular.4b99954f.woff2"
+  }
+]);
